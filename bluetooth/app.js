6c479aa5bcc40e22798f260764ac2cfa2c2ea450
adrianromeroopenbravo <adrianromeroopenbravo@users.noreply.github.com>
2018-07-20 11:34:40 +0200

    all devices

--- bluetooth/app.js
+++ bluetooth/app.js
@@ -3,8 +3,11 @@ var bluetoothPrinter = new BluetoothPrinter();
 document.getElementById('connect').addEventListener('click', event => {
   bluetoothPrinter.request()
   .then(_ => bluetoothPrinter.connect())
-  .then(_ => { 
-      bluetoothPrinter.writePrinter('Esto es una prueba\n');
-  })
   .catch(error => { alert(error) });
 });
+
+document.getElementById('print').addEventListener('click', event => {
+
+    bluetoothPrinter.writePrinter('Esto es una prueba\n')
+    .catch(error => { alert(error) });
+  });
