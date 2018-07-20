969a0a55e8de39f61f284c6e94f8af6aa03bd507
adrianromeroopenbravo <adrianromeroopenbravo@users.noreply.github.com>
2018-07-20 11:58:34 +0200

    encoding

--- bluetooth/bluetooth.js
+++ bluetooth/bluetooth.js
@@ -1,46 +1,55 @@
 class BluetoothPrinter {
 
     constructor() {
-      this.device = null;
-      this.onDisconnected = this.onDisconnected.bind(this);
+        this.device = null;
+        this.onDisconnected = this.onDisconnected.bind(this);
     }
-    
+
     request() {
-      let options = {
-        // "filters": [{
-            
-        // }],
-        "optionalServices": [0xe7810a7173ae499d8c15faa9aef0c3f2],
-        acceptAllDevices: true,
-      };
-      return navigator.bluetooth.requestDevice(options)
-      .then(device => {
-        this.device = device;
-        this.device.addEventListener('gattserverdisconnected', this.onDisconnected);
-      });
+        let options = {
+            // "filters": [{
+
+            // }],
+            "optionalServices": [0xe7810a71],
+            acceptAllDevices: true,
+        };
+        return navigator.bluetooth.requestDevice(options)
+            .then(device => {
+                this.device = device;
+                this.device.addEventListener('gattserverdisconnected', this.onDisconnected);
+            });
     }
-    
+
     connect() {
-      if (!this.device) {
-        return Promise.reject('Device is not connected.');
-      }
-      return this.device.gatt.connect();
+        if (!this.device) {
+            return Promise.reject('Device is not connected.');
+        }
+        return this.device.gatt.connect();
     }
-    
+
     writePrinter(data) {
-      return this.device.gatt.getPrimaryService(0xe7810a7173ae499d8c15faa9aef0c3f2)
-      .then(service => service.getCharacteristic(0xbef8d6c99c214c9eb632bd58c1009f9f))
-      .then(characteristic => characteristic.writeValue(data));
+        alert('printing');
+        return this.device.gatt.getPrimaryService(0xe7810a71)
+            .then(service => {
+                alert('got service');
+                service.getCharacteristic(0xbef8d6c9);
+            })
+            .then(characteristic => {
+                alert('got characteristic');
+                let encoder = new TextEncoder('utf-8');
+                let userDescription = encoder.encode(data);
+                return characteristic.writeValue(userDescription);                
+            });
     }
-  
+
     disconnect() {
-      if (!this.device) {
-        return Promise.reject('Device is not connected.');
-      }
-      return this.device.gatt.disconnect();
+        if (!this.device) {
+            return Promise.reject('Device is not connected.');
+        }
+        return this.device.gatt.disconnect();
     }
-  
+
     onDisconnected() {
-      console.log('Device is disconnected.');
+        alert('Device is disconnected.');
     }
-  }
+}
