47a64f1d7f73d5658b5d333c63b97b4525055e9a
adrianromeroopenbravo <adrianromeroopenbravo@users.noreply.github.com>
2018-07-24 15:00:03 +0200

    Translating code

--- bluetooth/bluetooth.js
+++ bluetooth/bluetooth.js
@@ -1,14 +1,3 @@
-/*
- ************************************************************************************
- * Copyright (C) 2018 Openbravo S.L.U.
- * Licensed under the Openbravo Commercial License version 1.0
- * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
- * or in the legal folder of this module distribution.
- ************************************************************************************
- */
-
-/*global OB, Promise  */
-
 (function () {
 
     window.OB = window.OB || {};
