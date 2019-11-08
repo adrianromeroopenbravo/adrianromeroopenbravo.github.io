7d02946263a7f2524704776d7f67b517d1e39955
adrianromeroopenbravo <adrianromeroopenbravo@users.noreply.github.com>
2019-11-08 20:41:52 +0100

    Add Web Hardware

---
+++ webhardware/precache-manifest.e4fbcf1a8a2866a7636df1f9f46249eb.js
@@ -0,0 +1,29 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "./1d11654c497b6c33c325668870795f0f.svg"
+  },
+  {
+    "revision": "c1da22c1e06f8538614a",
+    "url": "./bundle.js"
+  },
+  {
+    "revision": "270fd897b2fedb9a54e1",
+    "url": "./common.bundle.js"
+  },
+  {
+    "revision": "39707a95d04aae09349668468277195b",
+    "url": "./favicon.ico"
+  },
+  {
+    "revision": "5ea0ec3b6aa2c6116f4879a62a8fc501",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1da22c1e06f8538614a",
+    "url": "./main.css"
+  },
+  {
+    "revision": "9d63f555da2fdafd7c76",
+    "url": "./react.bundle.js"
+  }
+]);
